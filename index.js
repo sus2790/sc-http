a46787a236fbc7dd6e14f338f0c6777bb8494737
sus2790 <104747821+sus2790@users.noreply.github.com>
2023-08-28 13:10:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,27 +4,25 @@ class NitroBlock {
             "id": "NitroBlock",
             "name": "NitroBlock",
             "blocks": [{
-                    "opcode": "substringy",
-                    "blockType": "reporter",
-                    "text": "letters [num1] through [num2] of [string]",
-                    "arguments": {
-                        "num1": {
-                            "type": "number",
-                            "defaultValue": "2"
-                        },
-                        "num2": {
-                            "type": "number",
-                            "defaultValue": "5"
-                        },
-                        "string": {
-                            "type": "string",
-                            "defaultValue": "hello world"
-                        }
+                "opcode": "substringy",
+                "blockType": "reporter",
+                "text": "letters [num1] through [num2] of [string]",
+                "arguments": {
+                    "num1": {
+                        "type": "number",
+                        "defaultValue": "2"
+                    },
+                    "num2": {
+                        "type": "number",
+                        "defaultValue": "5"
+                    },
+                    "string": {
+                        "type": "string",
+                        "defaultValue": "hello world"
                     }
-                },
-            }],
-        "menus": { //we will get back to this in a later tutorial
-        }
-    };
+                }
+            }]
+        };
+    }
 }
 Scratch.extensions.register(new NitroBlock());
