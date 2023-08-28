d020a9763973ec7edcfab9e1392f326a8741657c
sus2790 <104747821+sus2790@users.noreply.github.com>
2023-08-28 13:21:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
-const ky = require("ky");
+import retus from "retus"
 
 class ScratchHTTP {
-    getInfo() {
-        return {
-            id: "ScratchHTTP",
-            name: "HTTP Requests",
-            blocks: [{
-                opcode: "httpRequest",
-                blockType: Scratch.BlockType.REPORTER,
-                text: "HTTP [method] [url]",
-                arguments: {
-                    method: {
-                        type: Scratch.ArgumentType.STRING,
-                        menu: "method",
-                        defaultValue: "GET"
-                    },
-                    url: {
-                        type: Scratch.ArgumentType.STRING,
-                        defaultValue: "https://www.random.org/strings/?num=1&len=10&digits=on&upperalpha=on&loweralpha=on&format=plain"
-                    }
-                }
-            }],
-            menus: {
-                method: ["GET", "HEAD", "POST", "PUT", "DELETE", "CONNECT", "OPTIONS", "TRACE", "PATCH"]
-            }
-        };
-    }
+	getInfo() {
+		return {
+			id: "ScratchHTTP",
+			name: "HTTP Requests",
+			blocks: [{
+				opcode: "httpRequest",
+				blockType: Scratch.BlockType.REPORTER,
+				text: "HTTP [method] [url]",
+				arguments: {
+					method: {
+						type: Scratch.ArgumentType.STRING,
+						defaultValue: "GET"
+					},
+					url: {
+						type: Scratch.ArgumentType.STRING,
+						defaultValue: "https://www.random.org/strings/?num=1&len=10&digits=on&upperalpha=on&loweralpha=on&format=plain"
+					}
+				}
+			}]
+		}
+	}
 
-    async httpRequest({method, url}) {
-        return await ky(url, {method}).text();
-    }
+	httpRequest({ method, url }) {
+		return retus(url, { method }).body
+	}
 }
 
-Scratch.extensions.register(new ScratchHTTP());
+Scratch.extensions.register(new ScratchHTTP())
