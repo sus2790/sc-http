c2d7ef8edc66d8e65f987875a3a6b693b6e3ec7f
sus2790 <104747821+sus2790@users.noreply.github.com>
2023-08-28 13:01:04 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+class NitroBlock {
+    getInfo() {
+        return {
+            "id": "NitroBlock",
+            "name": "NitroBlock",
+            "blocks": [{
+                    "opcode": "substringy",
+                    "blockType": "reporter",
+                    "text": "letters [num1] through [num2] of [string]",
+                    "arguments": {
+                        "num1": {
+                            "type": "number",
+                            "defaultValue": "2"
+                        },
+                        "num2": {
+                            "type": "number",
+                            "defaultValue": "5"
+                        },
+                        "string": {
+                            "type": "string",
+                            "defaultValue": "hello world"
+                        }
+                    }
+                },
+            }],
+        "menus": { //we will get back to this in a later tutorial
+        }
+    };
+}
+Scratch.extensions.register(new NitroBlock());
